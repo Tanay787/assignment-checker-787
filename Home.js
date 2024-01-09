04baefbef36ca63955c2045550c466374b048f31
Tanay787 <138036645+Tanay787@users.noreply.github.com>
2024-01-09 19:48:08 +0530

    Updated Home.js

--- Home.js
+++ Home.js
@@ -1,18 +1,35 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { useTheme, Button } from 'react-native-paper';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import { Button } from 'react-native-paper';
+import { Card } from 'react-native-paper';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import firebase from 'firebase';
 import AuthContext from './AuthContext';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
 const Home = ({ navigation }) => {
- const { uid, role } = useContext(AuthContext);
- const [storedRole, setStoredRole] = useState(null);
- const { colors } = useTheme();
+  const [imageDimensions, setImageDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
+  const { uid, role } = useContext(AuthContext);
+  const [storedRole, setStoredRole] = useState(null);
 
   useEffect(() => {
+    setImageDimensions({ width: windowWidth, height: windowHeight });
+
     const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         // User is logged in, navigate to the respective screen based on role
@@ -97,57 +114,79 @@ const Home = ({ navigation }) => {
     getStoredRole();
   }, []);
 
-   return (
-   <SafeAreaView style={styles.container}>
-     <Text style={styles.title}>Assignment Checker</Text>
-     <Text style={styles.tagline}>A better way to manage assignments</Text>
-     <View style={styles.buttonContainer}>
-       <Button
-         mode="contained"
-         icon={() => <Icon name="login" size={20} color="#fffffff" />}
-         onPress={handleLogin}
-         style={styles.button}>
-         Get Started
-       </Button>
-       <Button
-         mode="contained"
-         icon={() => <Icon name="account-plus" size={20} color="#e5e5e5ff" />}
-         onPress={handleSignup}
-         style={styles.button}>
-         Create Account
-       </Button>
-     </View>
-     {storedRole && <Text>Stored Role: {storedRole}</Text>}
-   </SafeAreaView>
- );
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#1e1e24" barStyle="light-content" />
+      <Image
+        source={require('./assets/assignment-checker.png')}
+        style={[styles.logo, imageDimensions, { resizeMode: 'contain' }]}
+      />
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Assignment Checker</Text>
+
+        <Text style={styles.tagline}>A better way to manage assignments</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          mode="contained"
+          icon={() => <Icon name="login" size={20} color="#fff" />}
+          onPress={handleLogin}
+          style={styles.button}>
+          Get Started
+        </Button>
+      </View>
+      {storedRole && (
+        <Text style={styles.roleText}>Stored Role: {storedRole}</Text>
+      )}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center',
-   backgroundColor: '#212529',
- },
- title: {
-   fontSize: 30,
-   color: '#e5e5e5ff',
-   fontFamily: 'Nexa',
-   marginBottom: 10,
- },
- tagline: {
-   fontSize: 16,
-   color: '#fca311ff',
-   marginBottom: 20,
- },
- buttonContainer: {
-   justifyContent: 'space-around',
-   width: '80%',
- },
- button: {
-   margin: 20,
-   backgroundColor: '#14213dff',
- },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1e1e24',
+  },
+  logo: {
+    flex: 0.8,
+    width: '100%',
+    height: '100%',
+    transform: [{ scale: 1.3 }],
+  },
+  titleContainer: {
+    justifyContent: 'flex-start',
+    width: '90%', // Add this line
+  },
+  title: {
+    fontSize: 25,
+    color: '#fca311ff',
+    fontFamily: 'Nexa',
+    marginBottom: 10,
+  },
+  tagline: {
+    fontSize: 28,
+    color: '#e5e5e5ff',
+    marginBottom: 20,
+    fontFamily: 'Nexa',
+  },
+  buttonContainer: {
+    justifyContent: 'space-around',
+    width: '60%',
+  },
+  button: {
+    margin: 20,
+    backgroundColor: '#2d2d38',
+    borderRadius: 30,
+    position: 'relative',
+    bottom: 10,
+  },
+  roleText: {
+    fontSize: 14,
+    color: '#9e9e9eff',
+    marginTop: 20,
+  },
 });
 
 export default Home;
